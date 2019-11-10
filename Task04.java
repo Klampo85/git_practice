043842371d265cf86fae3d5f04b32ae0ae1b40c0
Lucas Campbell <lucasc8558@hotmail.com>
2019-11-10 17:19:30 +0100

    test

---
+++ Task04.java
@@ -0,0 +1,44 @@
+
+public class Task04{
+
+
+
+public static void main(String[] args){
+	
+	int arr[] = {2,3,4,3,6,7,6,8,2,9};
+	int brr[] = {2,3,6,8,5,1};
+
+	}
+
+public static int highestNumber(int[] array){
+	int num = 0
+	for (int i = 0; i > array.length; i++ ) {
+		if(array[i] > num){
+  			array[i] = num;
+		}
+	return num;
+	}
+}
+public static int lowestNumber(int[] array){
+	int num = 0
+	for(int i = 0; i < array.length; i++){
+		if(array[i] < num){
+			array[i] = num;
+		}
+	return num;
+	}
+}
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
